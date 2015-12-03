3dc73d8f955adb6a507e608269e0d45d3d896642
Dan Griffin <unknown7979@gmail.com>
2015-12-03 11:55:02 -0500

    Added updates to data load module, as well as a partial sentiment analysis implementation with naive bayes.

--- SentimentAnalysisMain.py
+++ SentimentAnalysisMain.py
@@ -5,8 +5,24 @@ Created on Mon Nov 30 16:04:00 2015
 @author: DAN
 """
 
+from __future__ import division  # Needed in Python 2 for tokenizing.
 import nltk as nltk
 import dataLoadModule as dl
+from sklearn.cross_validation import train_test_split
+import gc
+
+#Sentiment analysis example.
+#Import a niave bayes classifier.
+from nltk.classify import NaiveBayesClassifier
+#Import the subjectivity corpus for training.
+from nltk.corpus import subjectivity
+#Import a sentiment analyzer to analyze documents.
+from nltk.sentiment import SentimentAnalyzer
+#Import the sentiment toolkit for email analysis.
+from nltk.sentiment.util import *
+
+#Tokenizer for tokenizing the emails.
+from nltk import word_tokenize
 
 
 #GLOBALS
@@ -16,14 +32,56 @@ def main():
     global data
     
     #Get all of the emails.
-    data = dl.getFullEmailData()
+    #data = dl.getFullEmailData()
     
+ 
+#def nltkSentimentAnalysisTutorial():
+#Get all subjective and objective sentences.
+#Note: The "encode/decode" statement is used to parse the unicode representation of the text to an 
+#Ascii representation. The "apply_features()" method throws an error if this isn't done. This is most 
+#likely because python 3 uses unicode characters to perform operations on string, while python 2 doesn't.
+subj_docs = [ [string.encode('ascii', 'ignore').decode('ascii') for string in sent] for sent in subjectivity.sents(categories='subj')]
+obj_docs = [ [string.encode('ascii', 'ignore').decode('ascii') for string in sent ] for sent in subjectivity.sents(categories='obj')]     
+#obj_docs = [(sent.encode('ascii', 'ignore').decode('ascii'), 'obj') for sent in subjectivity.sents(categories='obj')]    
+    
+#Randomly split data sets into train and test sets.
+train_subj, test_subj = train_test_split(subj_docs, test_size=100, train_size=200)
+train_obj, test_obj = train_test_split(obj_docs, test_size=100, train_size=200)
+    
+#Aggregate train and test data sets.
+train = train_subj + train_obj
+test = test_subj + test_obj
 
+#Create a sentiment analyzer to analyze the text documents. This analyzer 
+#provides an abstraction for managing a classifier, and feature extractor.
+#It also provides convinence data metrics on classifier performance. 
+sentim_analyzer = SentimentAnalyzer()
+#Mark negations in the tokenized training text, and count all negative words.
+#all_words() returns all tokens from the document, which is used to create a set 
+#of features with a feature extractor.
+all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in train])
+#Create the unigram features, only taking features that occur more than 4 time.
+unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)
+#Create a feature extractor based on the unigram word features created.
+#The unigram feature extractor is found in the sentiment utils package.
+sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+#Create feature-value representations of the data.
+train_set = sentim_analyzer.apply_features(train)
+test_set = sentim_analyzer.apply_features(test)
+#Collect unused memory before training.
+#Note, training may take a long time.
+gc.collect()
+#Create a trainer and train the sentiment analyzer on the training set.  
+print("Begining the classifier training...")
+trainer = NaiveBayesClassifier.train
+classifier = sentim_analyzer.train(trainer, train_set)
+print("Classifier Trained!")
+#Test the classifier on the test set.
+for key,value in sorted(sentim_analyzer.evaluate(test_set).items()):
+    print('{0}: {1}'.format(key, value))
 
-
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
 
 
 
--- dataLoadModule.py
+++ dataLoadModule.py
@@ -10,6 +10,12 @@ import pandas as pd
 import sqlite3 as sql
 import os
 
+#Import the libraries for parsing the email data into the same format as that 
+#used to train the classifier.
+from nltk import word_tokenize
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+
 
 data = None
 
@@ -37,6 +43,57 @@ def getNonEmptyEmailBodys():
     data = pd.DataFrame(dataDict)
     db.close();    
     return data
+    
+    
+'''
+Returns a pandas data frame with all of the email body text tokenized. This will take 
+a few seconds to run for 6741 emails with non-empty body text. This method breaks emails first 
+into sentences. Each sentence is then split into tokens. Then, each token between negations is 
+marked with the "_NEG" tag. This increases the accuracy of the classifier since negations are 
+very important to analyzing text. Finally, each email is stored as a list of tokenized sentences. 
+The resulting pandas DataFrame is returned with each row representing an email, and each entry in the column 
+representing a list of tokenized sentences from the email body. 
+
+NOTE!!!!!: This data hasn't been re-formatted in terms of the features used for sentiment analysis! This may 
+cause errors since the data isn't formatted in the same way. (Aka, if using unigram features, the document is represented 
+as a bag of words with a field representing True or False.)
+'''
+def getNonEmptyEmailBodysTokenized():
+    db = sql.connect("./data/database.sqlite")      
+    cursor = db.cursor()
+    #Used to mark all words that come between negations.
+    sentiment_analyzer = SentimentAnalyzer()
+    #Load the pre-trained sentence tokenizer.
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')    
+
+    #Get the keys for the pandas data frame.
+    cursor.execute('''PRAGMA table_info('Emails');''')
+    columnNames = []
+    for row in cursor:
+        columnNames.append(row[1])
+    
+    #Retrieve all email data.
+    cursor.execute('''SELECT ExtractedBodyText FROM Emails WHERE (ExtractedBodyText != '');''')
+    dataDict = { 'ExtractedBodyText': []}  
+    for row in cursor:
+        #Extract each column value and add it to the dictionary.
+        tokenizedToSentences = sent_detector.tokenize(row[0].strip())
+        tokenizedToWordsList = []
+        for sentence in tokenizedToSentences:
+            #Split sentences into words.
+            tokenizedWords = word_tokenize(sentence)
+            #Add negation tag to each word that has been negated, from the sentiment utils package.
+            tokenizedWordsWithNeg = mark_negation(tokenizedWords)
+            #Add newly tokenized word to list.
+            tokenizedToWordsList.append(tokenizedWordsWithNeg)
+        #Add list of tokenized sentences to dataDict.    
+        dataDict['ExtractedBodyText'].append(tokenizedToWordsList)
+
+    #Return pandas data frame with all data.
+    data = pd.DataFrame(dataDict)
+    db.close();    
+    return data    
+    
     
 '''
 Returns a pandas dataframe with all of the email content.
